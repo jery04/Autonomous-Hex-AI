5b98290d40359b37eccce3464d436d91b27b0462
Jery <jery.rf03@gmail.com>
2026-03-24 17:13:09 -0400

    Track only estrategia.pdf, solution.py, genetic_algorithm.py; update .gitignore

--- player.py
+++
@@ -1,8 +0,0 @@
-from board import HexBoard
-
-class Player:
-    def __init__(self, player_id: int):
-        self.player_id = player_id  # Tu identificador (1 o 2)
-
-    def play(self, board: HexBoard) -> tuple:
-        raise NotImplementedError("¡Implementa este método!")
